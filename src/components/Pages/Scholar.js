54556f84318f735a7aa5e999e7b9e5573ce3557e
Zunaid-jony <jony35666666@gmail.com>
2022-08-09 02:20:51 +0600

    exit

--- src/components/Pages/Scholar.js
+++ src/components/Pages/Scholar.js
@@ -1,15 +1,70 @@
-import React from 'react';
-import Footer from '../Footer/Footer';
-import Services from '../Home/Services';
+import React, { useState } from "react";
+
+import Footer from "../Footer/Footer";
+
 
 const Scholar = () => {
-    return (
-        <div>
-         
-             {/* <Services></Services> */}
-            <Footer></Footer>
-        </div>
-    );
+    // const [date, setDate] = useState(new Date())
+
+    // const onChange = date =>{
+    //     setDate(date)
+    // }
+  return (
+    <div>
+        <h2 className="text-center mt-5"> 🆂🅲🅷🅾🅻🅰🆁 🆂🅲🅷🅴🅳🆄🅻🅴</h2>
+        {/* <div style={{width:'100px'}} className="container mt-5 mb-5">
+            
+            <Calendar onChange={onChange} value={date}></Calendar>
+
+        </div> */}
+
+      <div className="container mt-5 mb-5">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>DATE</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>DELETE</th>
+            </tr>
+            <tr>
+              <td>df</td>
+              <td>df</td>
+              <td>df</td>
+              <td>des</td>
+            </tr>
+            <tr>
+              <td>df</td>
+              <td>df</td>
+              <td>df</td>
+              <td>des</td>
+            </tr>
+            <tr>
+              <td>df</td>
+              <td>df</td>
+              <td>df</td>
+              <td>des</td>
+            </tr>
+            <tr>
+              <td>df</td>
+              <td>df</td>
+              <td>df</td>
+              <td>des</td>
+            </tr>
+            <tr>
+              <td>df</td>
+              <td>df</td>
+              <td>df</td>
+              <td>des</td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
+      {/* <Services></Services> */}
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Scholar;
