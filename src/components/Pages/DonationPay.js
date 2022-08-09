efd80f7ebe441291ea3c8c185f51a1ce2b0901d5
Zunaid-jony <jony35666666@gmail.com>
2022-08-09 11:19:16 +0600

    pay google pay

--- src/components/Pages/DonationPay.js
+++ src/components/Pages/DonationPay.js
@@ -2,6 +2,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import CheckoutForm from './CheckoutForm';
+import GooglePay from './GooglePay';
 
 
 const stripePromise = loadStripe('pk_test_51LUZbMIqhxh87CUYWafDCf0kKoPFGP5J92pLT2qSlAf63n8jr9nB5SmI1iAOtqrfHflqMLnw2hMmIROu35XAhDHJ000thawa6w');
@@ -9,6 +10,7 @@ const stripePromise = loadStripe('pk_test_51LUZbMIqhxh87CUYWafDCf0kKoPFGP5J92pLT
 const DonationPay = () => {
     return (
         <div className='container'>
+                 
             <br></br>
             <img style={{width:'90%'}} src="https://i.ibb.co/vxRvsVf/333-3335493-follow-us-payment-method-icons-png.png" alt="333-3335493-follow-us-payment-method-icons-png" border="0"></img>
             {/* 
@@ -20,7 +22,10 @@ const DonationPay = () => {
              <Elements stripe={stripePromise}>
             <CheckoutForm/>        
         </Elements>
+
             </div>
+            
+ <GooglePay></GooglePay>
 
         </div>
     );
